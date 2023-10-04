e58f07acca51ee351b577b11e6037e6ba2ceb436
syachra <114629791+syachra@users.noreply.github.com>
2023-10-04 19:50:18 +0700

    Add files via upload

---
+++ Tugas2.js
@@ -0,0 +1,11 @@
+// identitas
+
+var firstName = "Syachra"
+var lastName = "Indyra puteri"
+var age = "18"
+var isMarried = "False"
+
+console.log(`FIRST NAME : ${firstName}`);
+console.log(`LAST NAME : ${lastName}`);
+console.log(`AGE : ${age}`);
+console.log(`iS MARRIED: ${isMarried}`);
